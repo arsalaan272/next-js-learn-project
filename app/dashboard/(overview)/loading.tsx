560cfeb7900bef2168ba3d413e0817a4e688e208
arsalaan272 <arsalaan272158@gmail.com>
2026-01-12 16:45:39 +0530

    refershing

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,25 @@
+"use client"
+import DashboardSkeleton from "../../ui/skeletons"
+
+
+
+// import React from 'react'
+
+// const loading = () => {
+//   return (
+//     <div>
+//       Loading...
+//     </div>
+//   )
+// }
+
+// export default loading
+
+// export default function loading() {
+//     return <div>Loading...</div>
+// }
+
+export default function Loading(){
+    return <DashboardSkeleton />
+    
+}
